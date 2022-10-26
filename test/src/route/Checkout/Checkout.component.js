34b91515eb031640237aa376f49a0efe280e73f4
maciej <szczepaniak.maciej1@gmail.com>
2022-10-26 18:17:02 +0200

    push

---
+++ test/src/route/Checkout/Checkout.component.js
@@ -0,0 +1,80 @@
+import { Checkout as SourceCheckout } from 'SourceRoute/Checkout/Checkout.component';
+import './Checkout.extension.style.scss';
+import ContentWrapper from 'Component/ContentWrapper';
+
+export class Checkout extends SourceCheckout {
+
+  renderProgressBar() {
+    return(
+      <div id="progress">
+        <div id='progress-bar'>
+          <div id='bar-done' className='done-1'></div>
+          <div id='bar-remaining' className='remaining-1'></div>
+        </div>
+        <ul id="progress-status">
+          <li className={`progress-step ${this.props.checkoutStep === 'SHIPPING_STEP' || this.props.checkoutStep === 'BILLING_STEP' || this.props.checkoutStep === 'DETAILS_STEP' ? 'active' : ''}`}>
+            <div className='progress-step-circle'>
+              <div className='progress-step-number'>1</div>
+            </div>
+            <span><b>Shipping</b></span>
+          </li>
+          <li className={`progress-step ${this.props.checkoutStep === 'BILLING_STEP' || this.props.checkoutStep === 'DETAILS_STEP' ? 'active' : ''}`}>
+            <div className='progress-step-circle'>
+              <div className='progress-step-number'>2</div>
+            </div>
+            <span><b>Review & Payments</b></span>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+
+  barProgression() {
+    let barDone = document.getElementById('bar-done');
+    let barRemaining = document.getElementById('bar-remaining');
+    let bar = document.getElementById('progress-bar');
+    if(!bar) return;
+
+    if(this.props.checkoutStep === 'BILLING_STEP') {
+      barDone.classList.remove('done-1');
+      barDone.classList.add('done-2');
+      barRemaining.classList.remove('remaining-1');
+      barRemaining.classList.add('remaining-2');
+    } else if(this.props.checkoutStep === 'DETAILS_STEP') {
+      barDone.classList.remove('done-2');
+      barDone.classList.add('done-3');
+      barRemaining.classList.remove('remaining-2');
+      barRemaining.classList.add('remaining-3');
+    }
+  }
+
+  render() {
+    this.barProgression();
+    return (
+        <main block="Checkout">
+          { this.renderProgressBar() }
+            <ContentWrapper
+              wrapperMix={ { block: 'Checkout', elem: 'Wrapper' } }
+              label={ __('Checkout page') }
+            >
+                { this.renderSummary(true) }
+                <div block="Checkout" elem="Step">
+                    { this.renderTitle() }
+                    { this.renderGuestForm() }
+                    { this.renderStep() }
+                    { this.renderLoader() }
+                </div>
+                <div>
+                    { this.renderSummary() }
+                    { this.renderPromo() }
+                    { this.renderCoupon() }
+                </div>
+            </ContentWrapper>
+        </main>
+    );
+  }
+
+}
+
+
+export default Checkout;
